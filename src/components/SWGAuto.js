bb2bf0fab53ec2a5c588011416bb8242ef7c4ffa
Anton Ulyanov <polecat5@yandex.ru>
2023-10-12 01:36:24 +0300

    step 1 (#3)

--- src/components/SWGAuto.js
+++ src/components/SWGAuto.js
@@ -1,7 +1,15 @@
 import React from "react";
+import cn from "classnames";
 
-export function SWGAuto({color = "#da0a0a"}) {
-  return <g className="car_path">
+/**
+ *
+ * @param {string} color
+ * @param {('idle','run')} status
+ * @return {Element}
+ * @constructor
+ */
+export function SWGAuto({color = "#da0a0a", status = 'idle'}) {
+  return <g className={cn(`car_${status}`)}>
     <rect x="0" y="-100" width="500" rx="20" height="200" fill={color} stroke="black" strokeWidth="3"/>
     <rect x="120" y="-100" width="300" rx="20" height="200" fill="#99f2da" stroke="black" strokeWidth="3"/>
     <rect x="180" y="-100" width="200" rx="20" height="200" fill={color} stroke="black" strokeWidth="3"/>
